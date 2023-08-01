169f87c3f7db0d1ae175b73cf92cbb8023cac080
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-08-01 01:23:41 -0300

    add styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { NavLateralComponent } from './nav-lateral/nav-lateral.component';
+import { CardsComponent } from './cards/cards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    NavComponent,
+    NavLateralComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
